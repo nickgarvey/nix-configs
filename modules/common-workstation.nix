268612def1818c6b53096cf70b30e4cf74355022
Nick Garvey <garvey.nick@gmail.com>
2025-12-23 11:52:04 -0800

    Update SOPS to use workstation key instead of host specific

--- modules/common-workstation.nix
+++ modules/common-workstation.nix
@@ -7,7 +7,7 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
-  sops.age.keyFile = "/home/ngarvey/.config/sops/age/keys.txt";
+  sops.age.keyFile = "/root/.config/sops/age/keys.txt";
 
   nixpkgs.config.allowUnfree = true;
 
