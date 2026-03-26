33b295cfec01bdbc0ea11ee40d5f4dde9de6b3ce
Nick Garvey <garvey.nick@gmail.com>
2026-03-25 23:13:26 -0700

    Attempt to work around GPU hang on framework13

--- hosts/framework13-laptop/configuration.nix
+++ hosts/framework13-laptop/configuration.nix
@@ -12,6 +12,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelParams = [
+    "amdgpu.sg_display=0"
+    "amdgpu.dcdebugmask=0x410"
+    "amdgpu.cwsr_enable=0"
+  ];
+
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
   networking.hostName = "framework13-laptop";
