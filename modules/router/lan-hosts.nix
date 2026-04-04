aaf4844d2d700ab8c0d6f3e97bb509705605a913
Nick Garvey <garvey.nick@gmail.com>
2026-04-03 22:37:32 -0700

    Remove truenas host

--- modules/router/lan-hosts.nix
+++ modules/router/lan-hosts.nix
@@ -17,7 +17,6 @@
     { hostname = "desktop-nixos"; mac = "bc:fc:e7:1c:40:0f"; ipv4 = "10.28.8.80"; }
     { hostname = "glkvm";         mac = "94:83:c4:bb:1c:0d"; ipv4 = "10.28.9.145"; }
     { hostname = "microatx";      mac = "9c:6b:00:af:e9:d0"; ipv4 = "10.28.12.108"; }
-    { hostname = "truenas";       mac = "bc:24:11:60:4d:67"; ipv4 = "10.28.12.16"; }
     { hostname = "k3s-node-1";    mac = "78:55:36:00:4c:c4"; ipv4 = "10.28.15.1"; }
     { hostname = "k3s-node-2";    mac = "78:55:36:00:47:f2"; ipv4 = "10.28.15.2"; }
     { hostname = "k3s-node-3";    mac = "78:55:36:00:4d:80"; ipv4 = "10.28.15.3"; }
